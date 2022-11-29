444881f67f9250e55463bdc2d9ed41e622aeb5f3
wtx9521 <79903781+wtx9521@users.noreply.github.com>
2022-11-29 14:49:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oizUn6IS_4272ZX8TJM5vf9bWa10',
+      id: 'oizUn6ChRbF_hR5EYksAUoaJgpd4'
+      'oizUn6IS_4272ZX8TJM5vf9bWa10',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'EawJOmLfVNdPD2AHbm6KT6ss_sL5yI9FEc6ZCvSc6yU',
       festivals: [
