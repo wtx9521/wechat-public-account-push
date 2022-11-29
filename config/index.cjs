72bb356cb4e9d0a6e0ae8af6480afffc7227f317
wtx9521 <79903781+wtx9521@users.noreply.github.com>
2022-11-29 10:15:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oizUn6IS_4272ZX8TJM5vf9bWa10',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'EawJOmLfVNdPD2AHbm6KT6ss_sL5yI9FEc6ZCvSc6yU',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
